eefb8072e0de28ef3c2d758e6a4dc0a34b891089
Davids0n2 <davidand.tor007@gmail.com>
2024-10-19 04:06:10 -0300

    Deixei meu menu mais bonito.

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function toggleMenu() {
     const menu = document.querySelector('.menu > ul');
     menu.classList.toggle('active');
   }
-  
+  function toggleMenu() {
+    const menu = document.querySelector('nav.menu');
+    const menuList = document.querySelector('.menu > ul');
+    menu.classList.toggle('active');
+    menuList.classList.toggle('active'); 
+  }
   window.addEventListener('scroll', function() {
     const menu = document.querySelector('nav.menu');
     if (window.scrollY > 150) {
